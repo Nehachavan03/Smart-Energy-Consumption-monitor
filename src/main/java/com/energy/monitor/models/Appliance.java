b390bd91c7fd461a37ccc74559fada9e66c7037f
Anika Prasad <prasadanika25@gmail.com>
2026-04-10 20:54:21 +0530

    Final integration fixes and Anika module update

--- src/main/java/com/energy/monitor/models/Appliance.java
+++ src/main/java/com/energy/monitor/models/Appliance.java
@@ -2,12 +2,25 @@ package com.energy.monitor.models;
 
 public class Appliance {
 
+    private String id;
     private String name;
     private double powerWatts;
 
+    // Constructor (your version)
     public Appliance(String name, double powerWatts) {
         this.name = name;
         this.powerWatts = powerWatts;
+    }
+
+    // Constructor (for test files)
+    public Appliance(String id, String name, double powerWatts) {
+        this.id = id;
+        this.name = name;
+        this.powerWatts = powerWatts;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
